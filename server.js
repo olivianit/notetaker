15b05bbceb3708f58174cd1519b1fbe103011a47
Aivi <olivianitzel@gmail.com>
2023-05-01 23:09:45 -0400

    var functions

--- server.js
+++ server.js
@@ -6,3 +6,24 @@ const express = require('express');
 const app = express();
 
 const allNotes = require('./db/db.json');
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+    res.json(allNotes.slice(1));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
